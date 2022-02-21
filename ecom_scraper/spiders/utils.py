288ae52cadb52efd918732f0bbcedad27600c29a
Hamid Nahli <hamidnahlirss@gmail.com>
2022-02-21 15:50:44 -0800

    dcompose and update url query function added

--- ecom_scraper/spiders/utils.py
+++ ecom_scraper/spiders/utils.py
@@ -1,4 +1,8 @@
+from plistlib import Dict
+from typing import Any
+
 import pandas as pd
+from urllib.parse import urlparse, parse_qsl, urlunparse, urlencode
 
 
 def convert_file_to_list(filelocation):
@@ -28,4 +32,17 @@ def save_as(data, data_type, extension):
         df.to_json(f'{data_type}.json', orient="records", indent=4)
 
 
-
+def get_next_url(url: str, param: str, nxt: int):
+    url_parse = urlparse(url)
+    query = url_parse.query
+    url_dict: Dict[str, Any] = dict(parse_qsl(query))
+    if isinstance(url_dict[param], list):
+        page = int(url_dict[param][0]) + nxt
+    else:
+        page = int(url_dict[param]) + nxt
+    params = {param: page}
+    url_dict.update(params)
+    url_new_query = urlencode(url_dict)
+    url_parse = url_parse._replace(query=url_new_query)
+    next_url = urlunparse(url_parse)
+    return next_url
